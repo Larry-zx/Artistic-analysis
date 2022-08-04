1d26fcec51281b00b30a2c42118974135784cd0e
Xin Zhong <1140091006@qq.com>
2022-08-04 13:32:26 +0800

    readme

--- result/训练中的Loss.py
+++ result/训练中的Loss.py
@@ -6,18 +6,18 @@ from ArtDataset_classifer.config import Attr_list
 
 model_type_list = ['resnet18', 'alexnet', 'vgg16']
 task_type_list = ['multi', 'single']
-
+dataset = 'wikiArt'
 attr_list = Attr_list
 
 def make_loss(model_type, task_type):
-    loss = pd.read_csv(os.path.join(task_type, model_type, 'loss.csv'))
+    loss = pd.read_csv(os.path.join( dataset ,task_type, model_type, 'loss.csv'))
     for attr in attr_list:
         attr_loss = np.array(loss[attr])
         x = np.arange(len(attr_loss))
         plt.plot(x, attr_loss)
         plt.title(attr + '_loss')
         plt.ylabel('loss')
-        # plt.savefig(os.path.join('pic/loss', "%s-%s-%s.png" % (model_type, task_type, attr)))
+        plt.savefig(os.path.join(dataset + '/res_img/loss', "%s-%s-%s.png" % (model_type, task_type, attr)))
         plt.show()
 
 
